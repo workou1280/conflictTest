1de21e06f2565330200da987ac059d1281628da0
workou1280 <64866635+workou1280@users.noreply.github.com>
2022-03-30 16:16:13 +0900

    Update crawler.py
    change from branch2

--- crawler.py
+++ crawler.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 res = requests.get("https://www.kw.ac.kr/ko/life/notice.jsp",headers = {'user-agent': 'my-app/0.0.1'})
 print(type(res))
 soup = BeautifulSoup(res.content, 'html.parser',from_encoding='utf-8')
-soup = soup.find_all('div', {"class":"board-text"})
+soup = soup.find_all('div', {"class":)
 f = open('bulletin.txt','w')
 for link in soup:
     f.write(str(link))
